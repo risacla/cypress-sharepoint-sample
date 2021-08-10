af37e4088a02ff1a0be3a3b18a63036ec29beb23
risacla <car@s-cubed.dk>
2021-08-10 16:01:51 +0200

    Fix test

--- cypress/integration/feature_a/brickheadz.spec.js
+++ cypress/integration/feature_a/brickheadz.spec.js
@@ -38,11 +38,11 @@ describe('SharePoint SPFx Testing', function() {
    */
   it('Validate if the new template is created', () => {
     cy.contains('New').click()
-    cy.wait(1000);
-    cy.get('CV_Template').click()
+    //cy.wait(1000);
+    //cy.get('#id__2-menu > div > ul > li:nth-child(3) > button').click()
     //cy.wait(1000);
 
-    //cy.contains('CV_Template').click()
+    cy.contains('CV_Template').click()
     
 
   });
